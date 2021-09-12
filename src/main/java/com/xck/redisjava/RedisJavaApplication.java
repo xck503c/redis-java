509bdd38fb04f9a5bcbeac0940c5362868c6eaca
xck503c <2198494838@qq.com>
2021-09-12 11:57:57 +0800

    Initial commit

---
+++ src/main/java/com/xck/redisjava/RedisJavaApplication.java
@@ -0,0 +1,13 @@
+package com.xck.redisjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xck/redisjava/RedisJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xck.redisjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
